824da06d1260765cf0823a22653a5ee413d1dd5f
jorgitodf <jspaiva.1977@gmail.com>
2018-09-04 16:06:07 -0300

    First Commit

---
+++ app/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class User extends Eloquent
+{
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password'];
+}
---
+++ src/Controller.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Framework;
+
+
+abstract class Controller
+{
+    
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
